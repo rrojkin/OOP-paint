924a1becd836de5e4539d32179b63559bc4d45b6
Anton Razhnou <toxihrojkih@gmail.com>
2025-03-09 22:04:57 +0300

    Triangle added + drawing reworked

--- OOP-paint/MainWindow.xaml.cs
+++ OOP-paint/MainWindow.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using OOP_paint.Models;
 
 namespace OOP_paint
@@ -24,6 +29,11 @@ namespace OOP_paint
         }
 
         private allShapes selectedShape = allShapes.none;
+        private ShapeBase currentShape;
+        private Point startPoint;
+        private bool isDrawing = false;
+        private List<ShapeBase> shapes = new List<ShapeBase>();
+
 
         public void shapeButtonClick(object sender, RoutedEventArgs e)
         {
@@ -42,7 +52,7 @@ namespace OOP_paint
                 case "EllipseButton":
                     selectedShape = allShapes.ellipse;
                     break;
-                case "PolygonButton":
+                case "TriangleButton":
                     selectedShape = allShapes.polygon;
                     break;    
                 case "PolylineButton":
@@ -68,7 +78,7 @@ namespace OOP_paint
                     EllipseButton.IsChecked = false;
                     return;        
                 case allShapes.polygon:
-                    PolygonButton.IsChecked = false;
+                    TriangleButton.IsChecked = false;
                     return;
                 case allShapes.polyline:
                     PolylineButton.IsChecked = false;
@@ -78,14 +88,207 @@ namespace OOP_paint
             }
         }
 
+        private void mainCanvas_RightMouseDown(object sender, MouseEventArgs e)
+        {
+            if (selectedShape != allShapes.polyline) return;
+
+            if (isDrawing && currentShape is Models.Polyline)
+            {
+                shapes.Add(currentShape);
+                isDrawing = false;
+                currentShape = null;
+            }
+        } 
+
+        private void mainCanvas_LeftMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Debug.WriteLine("Это сообщение для дебаггера.");
+
+            if (selectedShape == allShapes.none) return;
+
+            Point currentPoint = e.GetPosition(MainCanvas);
+
+            if (currentShape == null)
+            {
+                startPoint = currentPoint;
+                isDrawing = true;
+
+                switch (selectedShape)
+                {
+                    case allShapes.line:
+                        currentShape = new Models.Line()
+                        {
+                            StartPoint = startPoint,
+                            EndPoint = startPoint,
+                            Stroke = Brushes.White,
+                            StrokeThickness = 2
+                        };
+                        break;
+
+                    case allShapes.rect:
+                        currentShape = new Models.Rectangle
+                        {
+                            TopLeft = startPoint,
+                            Width = 0,
+                            Height = 0,
+                            Stroke = Brushes.White,
+                            StrokeThickness = 2,
+                        };
+                        break;
+
+                    case allShapes.ellipse:
+                        currentShape = new Models.Ellipse
+                        {
+                            TopLeft = startPoint,
+                            Width = 0,
+                            Height = 0,
+                            Stroke = Brushes.White,
+                            StrokeThickness = 2,
+                        };
+                        break;
+
+                    case allShapes.polygon:
+                        currentShape = new Models.Triangle()
+                        {
+                            Stroke = Brushes.White,
+                            StrokeThickness = 2,
+                            StartPoint = currentPoint,
+                            EndPoint = currentPoint
+                        };
+
+                        Debug.WriteLine("Done");
+                        break;
+
+                        currentShape = new Models.Polygon(new PointCollection { startPoint })
+                        {
+                            Stroke = Brushes.White,
+                            StrokeThickness = 2
+                        };
+                        break;
+
+                    case allShapes.polyline:
+                        currentShape = new Models.Polyline(new PointCollection { startPoint })
+                        {
+                            Stroke = Brushes.White,
+                            StrokeThickness = 2
+                        };
+                        break;
+                }
+            }
+            else
+            {
+                // Второй клик, конец рисования
+                if (selectedShape == allShapes.polygon)
+                {
+                    if (currentShape is Models.Triangle triangle)
+                    {
+                        
+                    }
+                }
+                else
+                {
+                    if (currentShape is Models.Line line)
+                    {
+                        line.EndPoint = currentPoint;
+                    }
+                    else if (currentShape is Models.Rectangle rect)
+                    {
+                        rect.Width = Math.Abs(currentPoint.X - startPoint.X);
+                        rect.Height = Math.Abs(currentPoint.Y - startPoint.Y);
+                        rect.TopLeft = new Point(Math.Min(startPoint.X, currentPoint.X), Math.Min(startPoint.Y, currentPoint.Y));
+                    }
+                    else if (currentShape is Models.Ellipse ellipse)
+                    {
+                        ellipse.Width = Math.Abs(currentPoint.X - startPoint.X);
+                        ellipse.Height = Math.Abs(currentPoint.Y - startPoint.Y);
+                        ellipse.TopLeft = new Point(Math.Min(startPoint.X, currentPoint.X), Math.Min(startPoint.Y, currentPoint.Y));
+                    }
+                    else if (currentShape is Models.Polyline polyline)
+                    {
+                        polyline.Points.Add(currentPoint);
+                    }
+                }
+
+                if (currentShape is not Models.Polyline || currentShape is Models.Triangle)
+                {
+                    shapes.Add(currentShape);
+                    currentShape = null;
+                    isDrawing = false;
+                }
+            }
+
+            RedrawCanvas();
+        }
+
+        private void mainCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isDrawing || currentShape == null) return;
+
+            Point endPoint = e.GetPosition(MainCanvas);
+
+            switch (currentShape)
+            {
+                case Models.Line line:
+                    line.EndPoint = endPoint;
+                    break;
+
+                case Models.Rectangle rect:
+                    rect.Width = Math.Abs(endPoint.X - startPoint.X);
+                    rect.Height = Math.Abs(endPoint.Y - startPoint.Y);
+                    rect.TopLeft = new Point(Math.Min(startPoint.X, endPoint.X), Math.Min(startPoint.Y, endPoint.Y));
+                    break;
+
+                case Models.Ellipse ellipse:
+                    ellipse.Width = Math.Abs(endPoint.X - startPoint.X);
+                    ellipse.Height = Math.Abs(endPoint.Y - startPoint.Y);
+                    ellipse.TopLeft = new Point(Math.Min(startPoint.X, endPoint.X), Math.Min(startPoint.Y, endPoint.Y));
+                    break;
+
+                case Models.Triangle triangle:
+                    triangle.EndPoint = endPoint;
+                    
+                    break;
+
+                    //if (polygon.Points.Count > 1)
+                    //{
+                    //    polygon.Points[polygon.Points.Count - 1] = endPoint; // Adjust last point while drawing
+                    //}
+
+                case Models.Polyline polyline:
+                    if (polyline.Points.Count > 1)
+                    {
+                        
+                        polyline.Points[polyline.Points.Count - 1] = endPoint; 
+                    } else
+                    {
+                        polyline.Points.Add(endPoint);
+                    }
+                    break;
+            }
+
+            // Перерисовываем холст
+            RedrawCanvas();
+        }
+
+        private void RedrawCanvas()
+        {
+            MainCanvas.Children.Clear(); 
+
+            foreach (var shape in shapes)
+            {
+                shape.Draw(MainCanvas);
+            }
+
+            if (currentShape != null)
+            {
+                currentShape.Draw(MainCanvas);
+            }
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void LineButton_Checked(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
--- OOP-paint/MainWindowModel.cs
+++ OOP-paint/MainWindowModel.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace OOP_paint.Models
 {
     public abstract class ShapeBase
     {
+        
         public Brush Stroke { get; set; } = Brushes.Black;
         public double StrokeThickness { get; set; } = 1;
-
         public abstract void Draw(Canvas canvas);
     }
 
     public class Line : ShapeBase
     {
+        
         public System.Windows.Point StartPoint { get; set; }
         public System.Windows.Point EndPoint { get; set; }
 
@@ -30,8 +34,9 @@ namespace OOP_paint.Models
                 X2 = EndPoint.X,
                 Y2 = EndPoint.Y,
                 Stroke = Stroke,
-                StrokeThickness = StrokeThickness
+                StrokeThickness = StrokeThickness,
             };
+            line.IsHitTestVisible = false;
             canvas.Children.Add(line);
         }
     }
@@ -53,34 +58,38 @@ namespace OOP_paint.Models
             };
             Canvas.SetLeft(rect, TopLeft.X);
             Canvas.SetTop(rect, TopLeft.Y);
+            rect.IsHitTestVisible = false;
             canvas.Children.Add(rect);
         }
     }
 
     public class Ellipse : ShapeBase
     {
-        public System.Windows.Point Center { get; set; }
-        public double RadiusX { get; set; }
-        public double RadiusY { get; set; }
+        public System.Windows.Point TopLeft { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
 
         public override void Draw(Canvas canvas)
         {
             var ellipse = new System.Windows.Shapes.Ellipse
             {
-                Width = RadiusX * 2,
-                Height = RadiusY * 2,
+                Width = Width,
+                Height = Height,
                 Stroke = Stroke,
                 StrokeThickness = StrokeThickness
             };
-            Canvas.SetLeft(ellipse, Center.X - RadiusX);
-            Canvas.SetTop(ellipse, Center.Y - RadiusY);
+            Canvas.SetLeft(ellipse, TopLeft.X);
+            Canvas.SetTop(ellipse, TopLeft.Y);
+            ellipse.IsHitTestVisible = false;
             canvas.Children.Add(ellipse);
         }
     }
 
     public class Polygon : ShapeBase
     {
-        public PointCollection Points { get; set; }
+        public PointCollection Points { get; set; } = new PointCollection();
+
+        public Polygon() {}
 
         public Polygon(PointCollection points)
         {
@@ -95,13 +104,16 @@ namespace OOP_paint.Models
                 Stroke = Stroke,
                 StrokeThickness = StrokeThickness
             };
+            polygon.IsHitTestVisible = false;
             canvas.Children.Add(polygon);
         }
     }
 
     public class Polyline : ShapeBase
     {
-        public PointCollection Points { get; set; }
+        public PointCollection Points { get; set; } = new PointCollection();
+
+        public Polyline() {}
 
         public Polyline(PointCollection points)
         {
@@ -116,6 +128,35 @@ namespace OOP_paint.Models
                 Stroke = Stroke,
                 StrokeThickness = StrokeThickness
             };
+            polyline.IsHitTestVisible = false;
+            canvas.Children.Add(polyline);
+        }
+    }
+
+    public class Triangle: Polyline
+    {
+        public System.Windows.Point StartPoint { get; set; }
+        public System.Windows.Point EndPoint { get; set; }
+        public override void Draw(Canvas canvas)
+        {
+        
+
+            var polyline = new System.Windows.Shapes.Polyline
+            {
+                
+
+                Points = new PointCollection()
+                {
+                    StartPoint,
+                    new Point(EndPoint.X, StartPoint.Y),
+                    new Point((StartPoint.X - (StartPoint.X - EndPoint.X)/2), StartPoint.Y + (StartPoint.X - EndPoint.X)),
+                    StartPoint
+                },
+                Stroke = Stroke,
+                StrokeThickness = StrokeThickness
+            };
+
+            polyline.IsHitTestVisible = false;
             canvas.Children.Add(polyline);
         }
     }
